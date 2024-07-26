20009681e902c846a918baaa764cad940f5992cb
QubitPi <jack20220723@gmail.com>
2024-07-26 12:07:55 +0000

    deploy: d4a47c925fa33f894228db1b0dabac57c9053ed6

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0.0.dev0+1265.g312c6dd5a2',
+    VERSION: '3.0.0.dev0+1266.gd4a47c925f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
